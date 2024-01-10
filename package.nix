1e6c47c88717755e2fbd4407a3218fa9362a8fde
samularity <samuelmunz@gmx.de>
2024-01-10 13:54:05 +0100

    first try 32bit

--- package.nix
+++ package.nix
@@ -1,11 +1,14 @@
 { lib
 , stdenv
 , cmake
+, pkg-config
+, glibc_multi
 }:
 
 stdenv.mkDerivation {
   name = "cmake-nix";
   src = ./.;
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake pkg-config  ];
+  buildInputs = [ glibc_multi ];
   installPhase = ''install -Dm755 $name $out/bin/$name'';
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <errno.h>
 
 int main(int argc, char** argv){
 	std::cout << "Hello NIX" << std::endl;
